c6739065b98435c5a34484e67ba1c2ff2419a9f8
Casper Huang <casper.w.huang@qq.com>
2025-04-19 14:09:22 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { allComponents, provideVSCodeDesignSystem } from '@vscode/webview-ui-toolkit'
-
-provideVSCodeDesignSystem().register(allComponents)
 </script>
 
 <template>
